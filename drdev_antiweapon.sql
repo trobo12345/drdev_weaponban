4dad3e19632149be4053167b4d29c214b7255986
Roby <goatkecske1512@gmail.com>
2023-12-28 18:49:46 +0100

    Update drdev_antiweapon.sql

--- drdev_antiweapon.sql
+++ drdev_antiweapon.sql
@@ -15,13 +15,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Struktúra mentése tábla revolutiontest. drdev_weaponban
-CREATE TABLE IF NOT EXISTS `drdev_weaponban` (
+CREATE TABLE IF NOT EXISTS `drdev_antiweapon` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-DELETE FROM `drdev_weaponban`;
+DELETE FROM `drdev_antiweapon`;
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
