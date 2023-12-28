d404b404d20d46fa0b597cae05fb388e5a8ba039
Roby <goatkecske1512@gmail.com>
2023-12-28 16:08:24 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-name "drdev_antiweapon"
+name "drdev_weaponban"
 description "This script implement a system where specific players can be denied the use of weapons, and it maintains a list of denied users both on the server and client sides for synchronization. The server handles the database operations, while the client handles notifying and disarming players."
 author "Roby"
 version "1.0.0"
